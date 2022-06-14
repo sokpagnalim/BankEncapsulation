f04a955532081b37c0fe2e97a688da950f8c2695
Sokpagna Lim <sokpagnalim@gmail.com>
2022-06-14 03:07:23 -0700

    bankEncapsulationExercise1

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        
+        private double balance=0;
+        public void Deposit (double amount)
+        {
+           balance=balance+amount;
+        }
+        public void Withdraw (double amount)
+        {
+            balance = balance-amount;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+            Console.WriteLine("Enter deposit amount");
+            var amountToDeposit= double.Parse(Console.ReadLine());
+            account.Deposit(amountToDeposit);
+            Console.WriteLine($"Yous balance is now {account.GetBalance()}");
+
+        
+         }
+    }
+}
